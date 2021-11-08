543ffe6e3c395bcee1c42663c6ff6e7804db444c
Guilherme Gonzalez <gcgonzalez99@hotmail.com>
2021-11-08 08:18:59 -0300

    fix: corrigindo erros no CRUD de cargos

--- src/service/cargo/cargo.service.ts
+++ src/service/cargo/cargo.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, Inject } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { DeepPartial, Repository } from 'typeorm';
+import { QueryDeepPartialEntity } from 'typeorm/query-builder/QueryPartialEntity';
 import { CargoDto } from './dto/cargo.dto';
 
 @Injectable()
@@ -14,7 +15,7 @@ export class CargoService {
   }
 
   //resolver
-  async criarCargo(cargo: CargoDto): Promise<CargoDto> {
+  async criarCargo(cargo: DeepPartial<CargoDto>): Promise<CargoDto> {
     return await this.cargoRepository.save(cargo);
   }
 
@@ -30,8 +31,12 @@ export class CargoService {
     return await this.cargoRepository.findOne({ where: { id_cargo: id_cargo } });
   }
 
-  async atualizar(id_cargo: number, data: Partial<CargoDto>) {
-    await this.cargoRepository.update({ id_cargo }, data);
+  async atualizar(id_cargo: number, data: QueryDeepPartialEntity<CargoDto>): Promise<CargoDto> {
+    const element = await this.cargoRepository.findOneOrFail(id_cargo);
+    if (!element.id_cargo) {
+      console.error('Element não existe!');
+    }
+    await this.cargoRepository.update(id_cargo, data);
     return await this.cargoRepository.findOne({ id_cargo });
   }
 
