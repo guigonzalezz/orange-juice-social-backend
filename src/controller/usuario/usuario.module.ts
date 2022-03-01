ae358ba6db0ae7d2b0b65a2a49986922b2e6cf36
Guilherme Gonzalez <gcgonzalez99@hotmail.com>
2022-02-28 23:15:17 -0300

    feat: importacao das entidades

--- src/controller/usuario/usuario.module.ts
+++ src/controller/usuario/usuario.module.ts
@@ -12,14 +12,29 @@ import { UsuarioPontuacao } from 'src/repository/database/usuario/entidades/usua
 import { CargoModule } from '../cargo/cargo.module';
 import { CargoRepository } from 'src/repository/database/cargo/cargo.repository';
 import { UsuarioFollow } from 'src/repository/database/usuario/entidades/usuario_follow.entity';
+import { UsuarioDesafioConclusao } from 'src/repository/database/usuario/entidades/usuario_desafio_conclusao.entity';
+import { UsuarioQuizConclusao } from 'src/repository/database/usuario/entidades/usuario_quiz_conclusao.entity';
+import { UsuarioTrilhaConclusao } from 'src/repository/database/usuario/entidades/usuario_trilha_conclusao.entity';
+import { UsuarioBlogLeitura } from 'src/repository/database/usuario/entidades/usuario_blog_leitura.entity';
+import { UsuarioCursoConclusao } from 'src/repository/database/usuario/entidades/usuario_curso_conclusao.entity';
+import { FeedbackRepository } from 'src/repository/database/feedback/feedback.repository';
+import { LogPlataforma } from 'src/repository/database/feedback/entidades/log_plataforma.entity';
+import { UdcFeedbackNota } from 'src/repository/database/feedback/entidades/udc_feedback_nota.entity';
+import { UqcFeedbackNota } from 'src/repository/database/feedback/entidades/uqc_feedback_nota.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([UsuarioV2, UsuarioPerfil, UsuarioSocial, UsuarioPontuacao, UsuarioFollow]), HttpModule, CargoModule],
+  imports: [TypeOrmModule.forFeature([
+    UsuarioV2, UsuarioPerfil, UsuarioSocial, 
+    UsuarioPontuacao, UsuarioFollow, UsuarioDesafioConclusao,
+    UsuarioQuizConclusao, UsuarioBlogLeitura, UsuarioCursoConclusao,
+    UsuarioTrilhaConclusao, UdcFeedbackNota, UqcFeedbackNota
+  ]), HttpModule, CargoModule],
   controllers: [UsuarioController],
   providers: [
     JwtStrategy,
     UsuarioService,
     UsuarioRepository,
+    FeedbackRepository,
     CargoRepository
   ],
   exports: [UsuarioService, TypeOrmModule],
